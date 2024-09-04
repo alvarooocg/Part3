a192d75cb9b86e1f71fd573a90cd8a05d01b48fc
alvaro <alvaro04cg@gmail.com>
2024-09-04 19:51:50 +0200

    .

--- e3.9-3.11/e3.9-3.11/vite.config.js
+++ e3.9-3.11/e3.9-3.11/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://fullstackweb-ma9n.onrender.com',
         changeOrigin: true,
       },
     }
